402fd5e2e3f93fc5cb44d44a4afcf3912ca4ebf3
tapswap <tapswapmoneydex@gmail.com>
2021-05-15 14:56:50 +0800

    initial commit

---
+++ src/constants/tapsPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
+  cakeAddress: '0x56eab07247e3e6404ac90140F20bba61375d5C3C',
+  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  lpAddress: '0x455254e9f1846F70184F586B56dfFb82E5370dec'
+}
+
+export default priceContracts
